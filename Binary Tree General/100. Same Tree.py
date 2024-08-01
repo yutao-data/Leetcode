e705d38780f7f6d71dadeca287cbb005f7035d79
C-bean <realyutaochen@outlook.com>
2024-08-01 15:29:11 +0200

    Update Today's Practice on Leetcode Binary Tree

---
+++ Binary Tree General/100. Same Tree.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.right, q.right) and self.isSameTree(p.left, q.left)
