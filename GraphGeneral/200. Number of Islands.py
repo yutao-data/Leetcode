ffd08e954efe114cca861b4c7a4a495dbfd863a9
C-bean <realyutaochen@outlook.com>
2024-09-18 15:09:17 +0200

    Trie

---
+++ GraphGeneral/200. Number of Islands.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+
+        num_islands = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == "1":
+                    self.dfs(grid, i, j)
+                    num_islands += 1
+
+        return num_islands
+
+    def dfs(self, grid, r, c):
+        if (
+            r < 0
+            or c < 0
+            or r >= len(grid)
+            or c >= len(grid[0])
+            or grid[r][c] != "1"
+        ):
+            return
+        grid[r][c] = "0"
+
+        self.dfs(grid, r - 1, c)
+        self.dfs(grid, r + 1, c)
+        self.dfs(grid, r, c - 1)
+        self.dfs(grid, r, c + 1)
---
+++ Trie (Prefix Tree)/211. Design Add and Search Words Data Structure.py
@@ -0,0 +1,19 @@
+class WordDictionary:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.trie = {}
+
+    def addWord(self, word: str) -> None:
+        """
+        Adds a word into the data structure.
+        """
+        node = self.trie
+
+        for ch in word:
+            if not ch in node:
+                node[ch] = {}
+            node = node[ch]
+        node["$"] = True
